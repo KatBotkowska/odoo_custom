2cb287ca2e14c4098f9cd03ed2eedac9c2cdcad8
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-07-14 20:58:22 +0200

    archive function

--- sale_order_archive/models/models.py
+++ sale_order_archive/models/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import datetime
+import logging
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class sale_order_archive(models.Model):
     _name = 'sale_order_archive.sale_order_archive'
     _description = 'Sale order archive modul'
-    # _inherits = 'sale.order'
     _order = 'create_date, partner_id, user_id'
 
     name = fields.Char(string='Name', required=True)
@@ -18,13 +18,10 @@ class sale_order_archive(models.Model):
     currency_id = fields.Many2one('res.currency', string='Currency', required=True)
     order_line = fields.Integer(string='Count of order lines', required=True)
 
-    # @api.one
     def _archive_sale_orders(self):
-        query = 'SELECT name, create_date, partner_id, user_id, amount_total, currency_id, order_line ' \
-                'FROM sale.order WHERE create_date > %s'
-        query_params = datetime.date.today() - datetime.timedelta(days=7)
-        old_orders = self.env.cr.execute(query, query_params)
-        orders_to_archive=self.env['sale_order_archive.sale_order_archive'].search([('create_date', '>', query_params)])
+        border_date = datetime.datetime.now() - datetime.timedelta(days=7)
+        date = border_date.strftime("%Y-%m-%d")
+        orders_to_archive = self.env['sale_order_archive.sale_order_archive'].search([('create_date', '>', date)])
         if orders_to_archive:
             for set in orders_to_archive:
                 self.env['sale_order_archive.sale_order_archive'].create({
@@ -37,17 +34,6 @@ class sale_order_archive(models.Model):
                     'order_line': set['order_line']
                 })
             orders_to_archive.unlink()
-
-        if old_orders:
-            for set in old_orders.fetchall():
-                self.env['sale_order_archive.sale_order_archive'].create({
-                    'name': set['name'],
-                    'create_date': set['create_date'],
-                    'partner_id': set['partner_id'],
-                    'user_id': set['user_id'],
-                    'amount_total': set['amount_total'],
-                    'currency_id': set['currency_id'],
-                    'order_line': set['order_line']
-                })
-            self.env["sale.order"].search([('create_date', '>', query_params)]).unlink()
-
+            logging.info('Archive done')
+        else:
+            logging.info('No rows to archive')
