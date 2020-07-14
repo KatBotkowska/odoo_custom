a3e37cf740de54cbf26afae0b22ebcdfff5fb326
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-07-14 18:14:00 +0200

    cron, views

--- sale_order_archive/models/models.py
+++ sale_order_archive/models/models.py
@@ -1,50 +1,53 @@
 # -*- coding: utf-8 -*-
 import datetime
 
-
 from odoo import models, fields, api
 
 
 class sale_order_archive(models.Model):
     _name = 'sale_order_archive.sale_order_archive'
     _description = 'Sale order archive modul'
-    _inherit = 'sale.order'
-    _order = 'order_create_date, customer, sale_person'
+    # _inherits = 'sale.order'
+    _order = 'create_date, partner_id, user_id'
 
     name = fields.Char(string='Name', required=True)
-    order_create_date = fields.Date(string='Order create date', required=True)
-    customer = fields.Many2one('res.partner', string='Customer', required=True)
-    sale_person = fields.Many2one('res.users', string='Sale person', required=True)
-    order_total_amount = fields.Monetary(currency_field='order_currency', string='Order total amount', required=True)
-    order_currency = fields.Many2one('res.currency', string='Currency', required=True)
-    count_order_lines = fields.Integer(string='Count of order lines', required=True)
+    create_date = fields.Date(string='Order create date', required=True)
+    partner_id = fields.Many2one('res.partner', string='Customer', required=True)
+    user_id = fields.Many2one('res.users', string='Sale person', required=True)
+    amount_total = fields.Monetary(currency_field='currency_id', string='Order total amount', required=True)
+    currency_id = fields.Many2one('res.currency', string='Currency', required=True)
+    order_line = fields.Integer(string='Count of order lines', required=True)
 
+    # @api.one
     def _archive_sale_orders(self):
-        # today = datetime.date.today()
-        # date_condition = today-datetime.timedelta(days=7)
-        # old_orders = self.env['sale.order'].search()
-        older = self.env.cr.execute('SELECT '
-                                    'name, order_create_date, customer, sale_person, order_total_amount,'
-                                    'order_currency, count_order_lines FROM sale.order '
-                                    'WHERE order_create_date<datetime.date.today()-datetime.timedelta(days=7)')
-        if older:
-            # self.env.cr.execute('CREATE name, order_create_date, customer, sale_person, order_total_amount, '
-            #                     'order_currency, count_order_lines FROM sale.order')
-            for row in older:
+        query = 'SELECT name, create_date, partner_id, user_id, amount_total, currency_id, order_line ' \
+                'FROM sale.order WHERE create_date > %s'
+        query_params = datetime.date.today() - datetime.timedelta(days=7)
+        old_orders = self.env.cr.execute(query, query_params)
+        orders_to_archive=self.env['sale_order_archive.sale_order_archive'].search([('create_date', '>', query_params)])
+        if orders_to_archive:
+            for set in orders_to_archive:
                 self.env['sale_order_archive.sale_order_archive'].create({
-                    'name': row['name'],
-                    'order_create_date': row['order_create_date'],
-                    'customer':row['customer'],
-                    'sale_person':row['sale_person'],
-                    'order_total_amount':row['order_total_amount'],
-                    'order_currency':row['order_currency'],
-                    'count_order_lines':row['count_order_lines']
-            })
-            self.env.cr.execute('DELETE * FROM sale.order WHERE '
-                                'order_create_date<datetime.date.today()-datetime.timedelta(days=7)')
+                    'name': set['name'],
+                    'create_date': set['create_date'],
+                    'partner_id': set['partner_id'],
+                    'user_id': set['user_id'],
+                    'amount_total': set['amount_total'],
+                    'currency_id': set['currency_id'],
+                    'order_line': set['order_line']
+                })
+            orders_to_archive.unlink()
 
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+        if old_orders:
+            for set in old_orders.fetchall():
+                self.env['sale_order_archive.sale_order_archive'].create({
+                    'name': set['name'],
+                    'create_date': set['create_date'],
+                    'partner_id': set['partner_id'],
+                    'user_id': set['user_id'],
+                    'amount_total': set['amount_total'],
+                    'currency_id': set['currency_id'],
+                    'order_line': set['order_line']
+                })
+            self.env["sale.order"].search([('create_date', '>', query_params)]).unlink()
+
