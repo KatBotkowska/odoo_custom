5d0c596597332cd8646216faa2567d8f4f141857
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-07-19 23:08:41 +0200

    m2one models for create

--- sale_order_archive/models/models.py
+++ sale_order_archive/models/models.py
@@ -26,13 +26,19 @@ class sale_order_archive(models.Model):
             for set in orders_to_archive:
                 print(set)
                 lines = len(self.env['sale.order.line'].search([('order_id', '=', set['id'])])._ids)
+                currency_id = self.env['res.currency'].search([('currency_id', '=', set['currency_id'])]).id
+                user_id = self.env['res.users'].search([('user_id', '=', set['user_id'])]).id
+                partner_id = self.env['res.partner'].search([('partner_id', '=', set['partner_id'])]).id
                 self.env['sale_order_archive.sale_order_archive'].create({
                     'name': set['name'],
                     'create_date': set['create_date'],
-                    'partner_id': set['partner_id'],
-                    'user_id': set['user_id'],
+                    # 'partner_id': set['partner_id'],
+                    # 'user_id': set['user_id'],
+                    'partner_id':partner_id,
+                    'user_id':user_id,
                     'amount_total': set['amount_total'],
-                    'currency_id': self.env['res.currency'].search([('currency_id', '=', set['currency_id'])]).id,
+                    # 'currency_id': self.env['res.currency'].search([('currency_id', '=', set['currency_id'])]).id,
+                    'currency_id': currency_id,
                     'order_line': lines
                 })
             orders_to_archive.unlink()
